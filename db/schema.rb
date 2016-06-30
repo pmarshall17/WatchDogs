2ba2deaaa9d0b6fd9d3b5c277d5c7e7590bf51e4
Pat Marshall <pmarshall1716@gmail.com>
2016-06-29 20:28:32 -0600

    route shells added to home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629193730) do
+ActiveRecord::Schema.define(version: 20160630021934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20160629193730) do
   end
 
   create_table "compilations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
