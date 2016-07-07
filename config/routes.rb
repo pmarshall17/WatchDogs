86b746d001ee3c684f47bf4938b22a6bb61ddd1c
Shaws <seanrichardparker@gmail.com>
2016-07-07 17:31:06 -0600

    container width on larger screens and fonts overwrite testing

--- config/routes.rb
+++ config/routes.rb
@@ -6,26 +6,18 @@ Rails.application.routes.draw do
     resources :favorites
   end
 
-  
-    
+
+
   devise_for :users
 
   get 'dogs/index'
-
   get 'dogs/home'
-
   get 'dogs/show'
-
   get 'dogs/edit'
-
   get 'dogs/new'
-
   get 'statics/meet_the_team'
-
   get 'statics/get_involved'
-
   get 'statics/breeds_traits'
-
   get 'statics/info_page'
 
   # GET QUIZZES
