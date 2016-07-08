b694b0846cd320dc58041361dd293d7644012b80
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-07-08 17:54:51 -0600

    quiz params

--- Gemfile
+++ Gemfile
@@ -31,7 +31,8 @@ gem 'httparty', '~>0.13.7'
 
 gem 'geocoder', '~>1.3.7'
 
-gem 'will_paginate', '~>3.1.0'
+gem 'will_paginate-materialize'
+gem 'will_paginate'
 
 
 
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,89 +5,22 @@ class QuizzesController < ApplicationController
     @results = petfinder.find_pets('dog', 84111, count: 25)
   end
 
-  # def index
-  #   @quizzes = Quiz.all
-  #   render :question1
-  # end
-
-  # def show
-  #   @quiz = Quiz.find(params[:id])
-  # end
-
-  # def edit
-  # end
-
-  # def new
-  #   @quiz = Quiz.new
-  # end
-
-  # def update
-  # end
-
-  # def create
-  #   @quiz = Quiz.new(quiz_params)
-
-  #   # respond_to do |format|
-  #   #   if @quiz.save
-  #   #     format.html { redirect_to @quiz, notice: 'Quiz was successfully created.' }
-  #   #     format.json { render :show, status: :created, location: @quiz }
-  #   #   else
-  #   #     format.html { render :new }
-  #   #     format.json { render json: @quiz.errors, status: :unprocessable_entity }
-  #   #   end
-  #   # end
-  # end
-
-  # def destroy
-  #   # @quiz.destroy
-  #   # respond_to do |format|
-  #   #   format.html { redirect_to quizzes_url, notice: 'Quiz was successfully destroyed.' }
-  #   #   format.json { head :no_content }
-  #   # end
-  # end
-
+  def form
+    binding.pry
+  end
+  
 
   # CUSTOM QUESTIONS ROUTES 
 
-  # def question_1
-  #   render :question1
-  # end
+  def question_1
+    render :question1
+  end
 
-  # def question_2
-  #   # @quiz = current_user.quiz 
-  #   # @quiz[:question_1] = params[:answer_1]
-  #   render :question2
-  # end
   
-  # def question_3
-  #   render :question3
-  # end
+
+
+
+  private
+
   
-  # def question_4
-  #   render :question4
-  # end
-
-  # def question_5
-  #   render :question5
-  # end
-
-  # def question_6
-  #   render :question6
-  # end
-
-  # def question_7
-  #   render :question7
-  # end
-
-
-
-  # private
-
-  # def quiz_params
-  #   params.require(:quiz).permit()
-  # end
-
-  # def quiz
-  #   @quiz = Quiz.find(params[:id])
-  # end
 end
