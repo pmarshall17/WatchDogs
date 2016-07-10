3ddcd25411485f540a61a26ce6c44ad73a3a1037
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-07-09 18:30:01 -0600

    beginning of form logic

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,26 +1,50 @@
 class QuizzesController < ApplicationController
 
   def results
-    petfinder = Petfinder::Client.new  
-    @results = petfinder.find_pets('dog', 84111, count: 25)
+    @options = Quiz.set_options(params) 
+    if @options[:size] == 'small'
+      @spaniel = Dog.search("#{params[:zip]}", {count: 5, breed: 'english springer spaniel'})
+      @maltese = Dog.search("#{params[:zip]}", {count: 5, breed: 'maltese'})
+      @shih_tzu = Dog.search("#{params[:zip]}", {count: 5, breed: 'shih tzu'})
+    end
+
+
+    if @options[:size] == 'large'
+      @beagle = Dog.search("#{params[:zip]}", {count: 5, breed: 'beagle'})
+      @spaniel = Dog.search("#{params[:zip]}", {count: 5, breed: 'english springer spaniel'})
+      @maltese = Dog.search("#{params[:zip]}", {count: 5, breed: 'maltese'})
+      @shih_tzu = Dog.search("#{params[:zip]}", {count: 5, breed: 'shih_tzu'})
+      @lab = Dog.search("#{params[:zip]}", {count: 5, breed: 'labrador retriever'})
+      @golden = Dog.search("#{params[:zip]}", {count: 5, breed: 'golden retriever'})
+      @bloodhounds = Dog.search("#{params[:zip]}", {count: 5, breed: 'bloodhound'})
+    end
+    
+    
+
+
+
+
+
   end
 
   def form
-    # binding.pry
-  end
-  
+    @params = params
+    redirect_to results_path(params: @params)
+  end  
 
   # CUSTOM QUESTIONS ROUTES 
 
   def question_1
-    
+
   end
 
   
-
-
-
   private
 
   
 end
+
+
+# def show
+#       @result = Dog.search("84102",{breed: "Labrador Retriever"})
+#     binding.pry
