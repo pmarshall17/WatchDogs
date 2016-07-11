1b2e5deefb949c5b9f222775ce71c9c0077cf264
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-07-11 16:34:20 -0600

    quiz logic almost finished

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,7 @@
 class DogsController < ApplicationController
 
   def show
-  	@result = Dog.search("84102",{age: "Young"})
+  	@result = Dog.search("84102",{age: "Young", breed: "shih tzu"})
   											# (params[:zip], params[:options]) the way to input the users data request search
   end
 end
