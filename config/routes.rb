8f8b944cb274e44f76aeadfe10279e5c2c7fbab5
Shaws <seanrichardparker@gmail.com>
2016-07-13 14:35:45 -0600

    giving omniauth callback controller access to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
   root 'compilations#index'
 
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   end
 
   resources :locations
-  
-  devise_for :users
 
   get 'search' => "locations#search"
 
