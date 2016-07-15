b63b370e91784c185a63074076e01057b6449a46
Pat Marshall <pmarshall1716@gmail.com>
2016-07-15 16:37:42 -0600

    fixed initializer omniauth

--- config/initializers/authomni.rb
+++ config/initializers/authomni.rb
@@ -1,8 +1,10 @@
-begin
-  omniauth = "#{Rails.root}/config/omniauth.yml"
-  YAML.load_file(omniauth).each do |key, value|
-    ENV[key] = value
-  end
-rescue
-  raise "no omniauth.yml config file in your config directory."
+if Rails.env.development?
+	begin
+	  omniauth = "#{Rails.root}/config/omniauth.yml"
+	  YAML.load_file(omniauth).each do |key, value|
+	    ENV[key] = value
+	  end
+	rescue
+	  raise "no omniauth.yml config file in your config directory."
+	end
 end
