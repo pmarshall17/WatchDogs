929afa05fec305df11c27df8c6ae80877e5eeb6f
Pat Marshall <pmarshall1716@gmail.com>
2016-07-15 15:40:03 -0600

    Map Markers Show info

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,10 +5,18 @@ class LocationsController < ApplicationController
     if params[:term]
       par = { term: params[:term], limit: 20 }
       @yelp_results = Yelp.client.search(params[:location], par)
-      @locations = @yelp_results.businesses.map { |business| business.location.coordinate  }
+      @locations = @yelp_results.businesses.map do |business| 
+        {
+         name: business.name,
+         address: business.location.address.join(" "),
+         latitude: business.location.coordinate.latitude,
+         longitude: business.location.coordinate.longitude
+        }  
+      end
       @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
-        marker.lat location.latitude
-        marker.lng location.longitude
+        marker.lat location[:latitude]
+        marker.lng location[:longitude]
+        marker.infowindow "<p>#{location[:name]}</p><p>#{location[:address]}</p>"
       end
     end
   end
