8c3b84bdb6e2347c8c4526383af160481d95f54e
Pat Marshall <pmarshall1716@gmail.com>
2016-07-16 11:31:18 -0600

    Heroku Charlie Foxtrot

--- refresh_token.rb
+++ refresh_token.rb
@@ -2,5 +2,5 @@ require 'digest'
 require 'pry'
 
 
-resp = `curl http://api.petfinder.com/auth.getToken -F key=3cb99c576efa43d0164ed946c198bc20 -F sig=7037d74bf5d5d181b6d6d096bec901a1`
+resp = `curl https://api.petfinder.com/auth.getToken -F key=3cb99c576efa43d0164ed946c198bc20 -F sig=7037d74bf5d5d181b6d6d096bec901a1`
 
