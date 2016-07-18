84c9638a4b0aa181607801a891d2d058f71028bd
Pat Marshall <pmarshall1716@gmail.com>
2016-07-18 15:11:23 -0600

    google maps api fix

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,6 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-navigator.geolocation.getCurrentPosition(function(position) {
-console.log(position.coords.latitude, position.coords.longitude);
+$(document).ready(function() {
 });
