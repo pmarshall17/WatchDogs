7a37b3ceed8f5545a89ae65ab4836ae954aed866
Pat Marshall <pmarshall1716@gmail.com>
2016-07-20 21:53:49 -0600

    contact us buttons added, sctollbar results added, nav left align

--- app/assets/javascripts/statics.js
+++ app/assets/javascripts/statics.js
@@ -2,3 +2,8 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "(801) 824-4072";
+}
