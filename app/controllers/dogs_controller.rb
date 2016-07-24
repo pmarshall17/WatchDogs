b9a021a2f26bb147851dafbf00e072d2b156b333
Pat Marshall <pmarshall1716@gmail.com>
2016-07-24 09:41:06 -0600

    ez link routes added

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,7 +3,7 @@ class DogsController < ApplicationController
   def show
   	options = {}
   	# 1. check to see if you have each param
-	options[:breed] = params[:breed] if params[:breed]
+		options[:breed] = params[:breed] if params[:breed]
   	options[:sex] = params[:sex] if params[:sex]
   	options[:size] = params[:size] if params[:size]
   	options[:age] = params[:age] if params[:age]
